aa25ee0c1d7bc5bfc832059272887091480d5455
Thomas Marrazzo <marrathomas18@gmail.com>
2022-07-20 20:58:43 -0300

    modifique carrito de compras

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let carrito = [];
+/* let carrito = [];
 productos();
 
 function productos () {
@@ -80,10 +80,98 @@ function valorFinal (cantidadProducto){
     alert (`el valor total a abonar es ${cantidadProducto}`);
 }
 
+ */
 
+const productos = [
+    { nombre: "perfumina vivex", precio: 400},
+    { nombre: "perfumina limon", precio: 400},
+    { nombre: "perfumina citrus", precio: 400},
+    { nombre: "perfumina floral", precio: 400},
+    { nombre: "perfumina jazmin", precio: 400},
+    { nombre: "difusor frutivex", precio: 530},
+    { nombre: "difusor katmandu", precio: 530},
+    { nombre: "difusor chiky", precio: 530},
+    { nombre: "difusor te verde", precio: 530},
+    { nombre: "difusor rosa jazmin", precio: 530},
+];
+let carrito = [];
 
+let seleccion = prompt("Hola ¿Desea comprar algun producto?");
 
+while(seleccion != "si" && seleccion != "no"){
+    alert("ingresar si o no")
+    seleccion = prompt("¿Desea comprar algun producto?")
+}
 
+if(seleccion == "si"){
+    alert("A continuacion la lista de prodcutos disponibles")
+    let todoslosProductos = productos.map((producto)=> `${producto.nombre} ${producto.precio}$`);
+    alert(todoslosProductos.join(" - "));
+}else if (seleccion == "no"){
+    alert("Gracias por su atencion!")
+}
+
+while(seleccion != "no"){
+    let producto = prompt("Agrega un producto a tu carrito");
+    let precio = 0;
+
+    if(producto == "perfumina Vivex" || producto == "perfumina limon" || producto == "perfumina citrus"
+    || producto == "perfumina Floral" || producto == "perfumina jazmin" || producto == "Difusor frutivex" 
+    || producto == "difusor katmandu" || producto == "difusor chiky" || producto == "Difusor te verde"
+    || producto == "difusor rosa jazmin"){
+        switch(producto){
+            case "perfumina vivex":
+                precio = 400;
+                break;
+            case "perfumina limon":
+                precio = 400;
+                break;
+            case "perfumina floral":
+                precio = 400;
+                break;
+            case "perfumina citrus":
+                precio = 400;
+                break;
+            case "perfumina jazmin":
+                precio = 400;
+                break;
+            case "difusor katmandu":
+                precio = 530;
+                break;
+            case "difusor frutivex":
+                precio = 530;
+                break;
+            case "difusor chiky":
+                precio = 530;
+                break;
+            case "difusor te verde":
+                precio = 530;
+                break;
+            case "difusor rosa jazmin":
+                precio = 530;
+                break;
+            default:
+                break;  
+        }
+        let unidades = parseInt(prompt("¿Cuantas unidades desea agregar?"));
+        carrito.push({producto, unidades, precio})
+        console.log(carrito)
+
+    }else{
+        alert("No disponemos de ese producto");
+    }
+    seleccion = prompt("¿Desea seguir comprando?");
+    while(seleccion == "no"){
+        alert("Gracias por la compra! Hasta pronto!");
+        carrito.forEach(carritoFinal => {
+            console.log(`producto: ${carritoFinal.producto} unidades: ${carritoFinal.unidades} Total a pagar: ${carritoFinal.unidades * carritoFinal.precio}`)
+        })
+    break;
+    }
+}
+
+const total = carrito.reduce((acc, el) => acc + el.precio * el.unidades, 0);
+console.log(`El total de su compra es: ${total}`);
 
 
 
